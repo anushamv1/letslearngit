07cf00184be1497a41a63c80d0872252aa6e30f3
anusha vernekar <asdfgka46@gmail.com>
2023-10-14 12:28:36 +0530

    changes made in multiply.java

--- multiply.java
+++ multiply.java
@@ -2,7 +2,7 @@ public class Multiply
 {
     public static void main(String[] args) {
         int a=10;
-        int b=20;
-        System.out.println("multiplication of two numbers is"+ a*b);
+        int c=20;
+        System.out.println("multiplication of two numbers is"+ a*c);
     }
 }
